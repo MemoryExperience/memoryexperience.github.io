8c7757d0bb743e517a9443a67c7eccb8d93c4fad
Enrique Arana <memoryx2023@gmail.com>
2023-12-27 13:01:04 -0500

    Diferentes contenidos a las imágenes de Carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
 const app = document.getElementById('typewriter');
+const app2 = document.getElementById('typewriter2');
+const app3 = document.getElementById('typewriter3');
 
 const typewriter = new Typewriter(app,{loop: true, delay: 75});
+const typewriter2 = new Typewriter(app2,{loop: true, delay: 75});
+const typewriter3 = new Typewriter(app3,{loop: true, delay: 75});
 
 typewriter.typeString('MEMORY EXPERIENCE').pauseFor(300).start();
+typewriter2.typeString('APRENDE SIN LÍMITES').pauseFor(300).start();
+typewriter3.typeString('APRENDE A ESTUDIAR').pauseFor(300).start();
